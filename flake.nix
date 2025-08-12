2e0a0f607761183a31a771aa71c5eba2bd220463
lemmih <lemmih@gmail.com>
2025-08-12 14:52:08 +0200

    clean up nix flake

--- flake.nix
+++ flake.nix
@@ -17,17 +17,35 @@
 
         node-modules = pkgs.mkYarnPackage {
           name = "node-modules";
-          src = ./.;
+          src = pkgs.lib.cleanSourceWith {
+            src = ./.;
+            filter = path: type:
+              baseNameOf path
+              == "package.json"
+              || baseNameOf path == "yarn.lock";
+          };
         };
 
         frontend = pkgs.stdenv.mkDerivation {
           name = "frontend";
-          src = ./.;
+          src = pkgs.lib.cleanSourceWith {
+            src = ./.;
+            filter = path: type: let
+              baseName = baseNameOf path;
+            in
+              pkgs.lib.hasSuffix ".pug" baseName
+              || pkgs.lib.hasSuffix ".less" baseName
+              || pkgs.lib.hasSuffix ".js" baseName
+              || pkgs.lib.hasSuffix ".ttf" baseName
+              || pkgs.lib.hasSuffix ".md" baseName
+              || pkgs.lib.hasSuffix ".parcelrc" baseName
+              || baseName == "package.json";
+          };
           buildInputs = [pkgs.yarn node-modules];
 
           buildPhase = ''
             ln -s ${node-modules}/libexec/rgeometry.github.io/node_modules node_modules
-            yarn parcel build index.pug
+            ${pkgs.yarn}/bin/yarn parcel build index.pug
           '';
 
           installPhase = ''
@@ -41,6 +59,19 @@
           node-modules = node-modules;
           # Default package
           default = frontend;
+        };
+
+        # Apps
+        apps = {
+          serve = {
+            type = "app";
+            program = toString (pkgs.writeShellScript "serve-frontend" ''
+              set -euo pipefail
+              echo "Starting development server..."
+              ${pkgs.yarn}/bin/yarn install
+              exec ${pkgs.yarn}/bin/yarn parcel serve index.pug
+            '');
+          };
         };
       }
     );
